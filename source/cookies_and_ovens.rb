8aab0a834c539e14e0f4a2eb2aaeda3e8fa648c3
Henry Hsu & Kevin Yeh <pair+henryh28.kyeh@devbootcamp.com>
2014-01-14 18:48:50 -0800

    Release 2: Add and refactor cookie state features and oven functions

--- source/cookies_and_ovens.rb
+++ source/cookies_and_ovens.rb
@@ -10,17 +10,46 @@
 
 # Your code here
 
-class Cookies
+class Cookie
+  DONENESS_STATES = [:doughy, :almost_ready, :ready, :burned]
 
-  def initialize(diameter=3, type="chocolate chip")
-    @doneness = "uncooked"
+  def initialize(diameter=3, type="chocolate chip", baking_time=10)
     @diameter = diameter
     @type = type
+    @baking_time = baking_time
+    @doneness = 0
+  end
+
+  def cook
+    @doneness +=1 if @doneness < DONENESS_STATES.length-1
+  end
+
+  def doneness
+    DONENESS_STATES[@doneness]
   end
 
 
 end
 
+
+class MacadamiaCookie < Cookie
+  def initialize
+    super(3, "Macadamia", 5)
+  end
+
+end
+
+class OatmealCookie < Cookie
+  def initialize
+    super(3, "Oatmeal", 8)
+  end
+end
+
+class SugarCookie < Cookie
+  def initialize
+    super(3, "Sugar", 7)
+  end
+end
 
 
 class Oven
@@ -33,21 +62,39 @@ class Oven
 
   def insert_cookies(batch)
     @rack.push(batch)
-
   end
 
   def remove_cookies(batch)
     @rack.delete(batch)
   end
 
+  def cook
+    if @rack.length != 0
+      @rack.each do |batch|
+        batch.each do |cookie|
+          cookie.cook
+        end
+      end
+    end
+  end
+
 
 end
 cookie_batch =[]
-12.times {cookie_batch << Cookies.new}
+12.times {cookie_batch << Cookie.new}
+
+macadamia_batch = []
+12.times {macadamia_batch << MacadamiaCookie.new}
+
+oatmeal_batch = []
+12.times {oatmeal_batch << OatmealCookie.new}
+
+sugar_batch = []
+12.times {sugar_batch << SugarCookie.new}
+
 
 p cookie_batch.inspect
-# yum = Cookies.new
-# p yum.inspect
+
 
 my_oven = Oven.new
 
@@ -57,7 +104,17 @@ my_oven.remove_cookies(cookie_batch)
 p my_oven.rack.length == 0
 
 
+my_oven.insert_cookies(oatmeal_batch)
+my_oven.insert_cookies(sugar_batch)
+my_oven.insert_cookies(macadamia_batch)
 
+my_oven.remove_cookies(sugar_batch)
+
+p my_oven.rack[0].first.doneness == :doughy
+my_oven.cook
+p my_oven.rack[0].first.doneness == :almost_ready
+my_oven.cook
+p my_oven.rack[0].first.doneness == :ready
 
 
 
