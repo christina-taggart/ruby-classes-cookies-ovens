09a8dea532f68977e3607f16904c785ce1a8e3fc
Darcey Lachtman & Patrick Vilhena <pair+dslachtman.nifengle@devbootcamp.com>
2014-01-14 17:05:04 -0800

    Cookies and Oven have harmonious pairing. Release 1 to come

--- source/cookies_and_ovens.rb
+++ source/cookies_and_ovens.rb
@@ -8,3 +8,54 @@
 
 
 # Your code here
+module Oven
+  def ready_to_remove?
+    @baked
+  end
+
+  def bake!
+    @baked = true
+  end
+end
+
+class Cookie
+  include Oven
+  attr_reader :flavor, :in_oven
+  def initialize(flavor)
+    @flavor = flavor
+    @in_oven = false
+    @baked = false
+  end
+
+  def put_in_oven
+    puts "You put some #{@flavor} cookies in the oven."
+  end
+
+  def remove_from_oven
+    puts "You removed some #{@flavor} cookies from the oven."
+  end
+end
+
+class ChocoChip < Cookie
+  def initialize
+    super("Chocolate chip")
+  end
+end
+
+choc_chip = ChocoChip.new
+choc_chip.put_in_oven
+choc_chip.bake!
+choc_chip.remove_from_oven
+
+class PeanutButter < Cookie
+  def initialize
+    super("Peanut Butter")
+  end
+end
+
+class SnickerDoodle < Cookie
+  def initialize
+    super("Snickerdoodle")
+  end
+end
+
