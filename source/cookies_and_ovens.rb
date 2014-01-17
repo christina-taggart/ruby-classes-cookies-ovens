66f8ebfb119da46ddfcc02dbc275d0b1bee617cc
Elliot Chalfant <null>
2014-01-17 08:45:14 -0800

    Almost finished with release 1; error on line 17

--- source/cookies_and_ovens.rb
+++ source/cookies_and_ovens.rb
@@ -1,94 +1,16 @@
-# Answer These Questions:
-
-# - What are essential classes?
-# - What attributes will each class have?
-# - What interface will each class provide?
-# - How will the classes interact with each other?
-# - Which classes will inherit from others, if any?
-
-
-# Your code here
-# class Oven
-#         @@ready = false
-#         def bake!
-#                 @@ready = true
-#         end
-#         def ready?
-#                 @@ready
-#         end
-# end
-
-# class Cookie < Oven
-#         def initialize(cookie_type)
-#                 @type = cookie_type
-#         end
-#         def make_cookie
-#                 self.bake!
-#                 self.ready?
-#                 p "Your #{@type} cookie is ready!"
-#         end
-# end
-
-# oven = Oven.new
-# choc_chip = Cookie.new("Chocolate Chip")
-# choc_chip.make_cookie
-
-
-# Nouns
-# - Cookies (types are included)
-# - Oven
-# - Batch
-
-# VERBS
-# - make (cookies)
-# - bake
-# - remove
-# - place (insert)
-
-# Oven
-# Methods - bake, place, remove
-# Attributes - timer?,
-
-# Cookie
-# Methods - make (initialize), place?, bake?
-# Attributes - type,
-# when to remove the cookie?
-
-# the oven holds cookies
-
-# INHERITANCE
-# "IS A"   -   A BAT IS A MAMMAL, A bus is a vehicle, An orange is a fruit
-#
-# COMPOSITION
-# "HAS A"  - A bike has gears,  A tree has oranges,
-# Oven has cookies
-# Batch has cookies
-#
-# class OrangeTree
-#    def initialize
-#             @oranges = []
-#    end
-#    def grow
-#       # make new oranges
-#    end
-# end
-
-
 class Oven
+  attr_reader :cookies_tray
   def initialize
     @cookies_tray = []
     @baked = false
   end
-  def insert_cookies(cookie)
-    @cookies_tray << cookie
+  def insert_cookies(batch)
+    @cookies_tray << batch
   end
-  def bake_cookies(cookie)
-    unless cookie.doneness == true
-      cookie.done?
+  def bake_cookies
+    @cookies_tray.each do |cookie|
+      p cookie
     end
-  end
-  def remove
-    @cookies_tray
   end
   def serve
     @cookies_tray.each do |cookie|
@@ -108,16 +30,22 @@ class Cookie
   end
 end
 
-# class MakeCookies
-#   def make
+class Batch
+  def initialize
+    p "What types of cookies would you like to make?"
+    p "Your choices are: "
+    p "Chocolate Chip"
+    p "and Fake cookie"
+    @input = gets.split(" ")
+    @batch = Array.new
+    @our_oven = Oven.new
+    @input.each do |cookie_type|
+      @batch << Cookie.new(cookie_type)
+    end
+    @our_oven.insert_cookies(@batch)
+    @our_oven.bake_cookies
+    @our_oven.serve
+  end
+end
 
-#   end
-# end
-
-
-oven = Oven.new
-cookie = Cookie.new("Chocolate Chip")
-oven.insert_cookies(cookie)  # need to initialize a cookie
-oven.bake_cookies(cookie)   # maybe a destructive method?
-oven.remove
-oven.serve
+Batch.new
